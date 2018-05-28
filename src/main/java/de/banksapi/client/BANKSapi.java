2ef08e3d2f2045e4addde41df08adce40d647039
Tobias König <tobias.koenig@banksapi.de>
2018-05-28 17:07:20 +0200

    more flexible base URL initialization, fixes #1

--- src/main/java/de/banksapi/client/BANKSapi.java
+++ src/main/java/de/banksapi/client/BANKSapi.java
@@ -4,27 +4,54 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.regex.Pattern;
 
+/**
+ * Initializes the BANKSapi base URL to be used by the reference client.
+ */
 public class BANKSapi {
 
     private static Pattern strWithVisibleCharacters = Pattern.compile(".*\\p{Graph}.*");
 
-    private static URL BANKSAPI_BASE_URL;
+    private static URL banksapiBaseUrl;
 
+    /**
+     * Retrieves the BANKSapi base URL.
+     * <p>If no URL has been set explicitly using {@link #init(URL)} the system property {@code BANKSAPI_BASE_URL} is
+     * used implicitly.</p>
+     *
+     * @return BANKSapi base URL
+     */
     public static URL getBanksapiBase() {
-        if (BANKSAPI_BASE_URL == null) {
-            String url = System.getProperty("BANKSAPI_BASE_URL");
-            url = isBlank(url) ? "https://banksapi.io" : url;
-            try {
-                BANKSAPI_BASE_URL = new URL(url);
-            } catch (MalformedURLException e) {
-                throw new IllegalStateException("Invalid URL '" + url + "'", e);
-            }
+        if (banksapiBaseUrl == null) {
+            initFromProperty();
         }
 
-        return BANKSAPI_BASE_URL;
+        return banksapiBaseUrl;
     }
 
-    public static boolean isBlank(String string) {
+    private static void initFromProperty() {
+        String url = System.getProperty("BANKSAPI_BASE_URL");
+        url = isBlank(url) ? "https://banksapi.io" : url;
+        try {
+            banksapiBaseUrl = new URL(url);
+        } catch (MalformedURLException e) {
+            throw new IllegalStateException("Invalid URL '" + url + "'", e);
+        }
+    }
+
+    /**
+     * Initializes the base URL with the given parameter.
+     *
+     * @param banksapiBaseUrl BANKSapi base URL
+     */
+    public static void init(URL banksapiBaseUrl) {
+        if (BANKSapi.banksapiBaseUrl == null) {
+            BANKSapi.banksapiBaseUrl = banksapiBaseUrl;
+        } else {
+            throw new IllegalStateException("init(URL) called but base URL is already initialized");
+        }
+    }
+
+    private static boolean isBlank(String string) {
         if (string == null) {
             return true;
         } else if (string.isEmpty()) {
